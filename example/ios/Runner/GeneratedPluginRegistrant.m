e92f09ce1a2e0050a52892b83e3591d04e83cf04
liangzg <292261829@qq.com>
2021-08-13 11:18:57 +0800

    ios

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<login_plugin/NBPlugin.h>)
+#import <login_plugin/NBPlugin.h>
+#else
+@import login_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NBPlugin registerWithRegistrar:[registry registrarForPlugin:@"NBPlugin"]];
+}
+
+@end
